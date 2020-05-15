6de9f589af97b5390e19dd45e6e239812f84b4b1
Justin Breneman <justin.breneman@yahoo.com>
2020-05-15 00:34:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,11 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash.map do |season, holiday_list|
+    if season == :winter
+      holiday_list.value.flatten
+    end
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
