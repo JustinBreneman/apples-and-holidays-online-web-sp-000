92e71dceb89d5c9a032e126ee12711c5ae120643
Justin Breneman <justin.breneman@yahoo.com>
2020-05-15 01:38:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday_list|
-    season.to_s.capitalize!
+    season.to_s
+    season.capitalize!
     puts "#{season}:"
   end
 end
